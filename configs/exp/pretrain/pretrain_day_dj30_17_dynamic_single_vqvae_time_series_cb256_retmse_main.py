ef68b570e430cd50e98fd23fe4e6e51efdd944ea
top-shen <confident_topshen@163.com>
2026-04-25 16:25:41 +0800

    增加了按return_mes

---
+++ configs/exp/pretrain/pretrain_day_dj30_17_dynamic_single_vqvae_time_series_cb256_retmse_main.py
@@ -0,0 +1,8 @@
+_base_ = [
+    "./pretrain_day_dj30_17_dynamic_single_vqvae_time_series_cb256.py"
+]
+
+tag = "single_vqvae_retmse_256"
+use_ema_for_eval = False
+best_metric = "ret_mse"
+best_metric_mode = "min"

0060ecbbb551e0bbf8dda23f1616b7c2e2e3bcd7
top-shen <confident_topshen@163.com>
2026-05-28 00:02:22 +0800

    修改tag

--- configs/exp/predict/predict_day_dj30_17_storm_lstm_rerun1337.py
+++ configs/exp/predict/predict_day_dj30_17_storm_lstm_rerun1337.py
@@ -1,5 +1,6 @@
 _base_ = ["./predict_day_dj30_17_storm_lstm.py"]
 
+workdir = "workdir"
 tag = "predict_day_dj30_17_storm_lstm_rerun1337"
 exp_path = f"{workdir}/{tag}"
 seed = 1337
--- configs/exp/predict/predict_day_dj30_17_storm_transformer_rerun1337.py
+++ configs/exp/predict/predict_day_dj30_17_storm_transformer_rerun1337.py
@@ -1,5 +1,6 @@
 _base_ = ["./predict_day_dj30_17_storm_transformer.py"]
 
+workdir = "workdir"
 tag = "predict_day_dj30_17_storm_transformer_rerun1337"
 exp_path = f"{workdir}/{tag}"
 seed = 1337

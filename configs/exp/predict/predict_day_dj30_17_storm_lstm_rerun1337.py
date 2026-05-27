7d18678b13f4efc920af8e0b4eacb43c0e7ae22a
top-shen <confident_topshen@163.com>
2026-05-27 23:40:05 +0800

    transformer和lstm seed=1337新增实验

---
+++ configs/exp/predict/predict_day_dj30_17_storm_lstm_rerun1337.py
@@ -0,0 +1,5 @@
+_base_ = ["./predict_day_dj30_17_storm_lstm.py"]
+
+tag = "predict_day_dj30_17_storm_lstm_rerun1337"
+exp_path = f"{workdir}/{tag}"
+seed = 1337
---
+++ configs/exp/predict/predict_day_dj30_17_storm_transformer_rerun1337.py
@@ -0,0 +1,5 @@
+_base_ = ["./predict_day_dj30_17_storm_transformer.py"]
+
+tag = "predict_day_dj30_17_storm_transformer_rerun1337"
+exp_path = f"{workdir}/{tag}"
+seed = 1337
